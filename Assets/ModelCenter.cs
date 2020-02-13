f1cfe06fc9a9b1c7f2724bfd8be6203d5635465d
Max Omdal <mjomdal@gmail.com>
2020-02-13 11:36:55 -0600

    fixed tracking

---
+++ Assets/ModelCenter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 avgCenter = Vector3.zero;
+        Vector3 avgLookAtRotation = Vector3.zero;
+        Vector3 avgLookUpRotation = Vector3.zero;
+        int enabledTrackerCount = 0;
+        for (int i = 0; i < TrackerObject.trackerObjects.Length; i++) {
+            TrackerObject trackerObj = TrackerObject.trackerObjects[i];
+            if (trackerObj.visible) {
+                print($"Tracking {trackerObj.posIdentifier}");
+                avgCenter += trackerObj.modelCenterApprox;
+                avgLookAtRotation += trackerObj.transform.up;
+                avgLookUpRotation += trackerObj.transform.forward;
+                enabledTrackerCount += 1;
+            }
+        }
+        if (enabledTrackerCount == 0) {
+            return;
+        }
+        avgCenter = avgCenter/enabledTrackerCount;
+        avgLookAtRotation = avgLookAtRotation/enabledTrackerCount;
+        gameObject.transform.position = avgCenter;
+        gameObject.transform.rotation = Quaternion.LookRotation(avgLookAtRotation, avgLookUpRotation);
+    }
+}
---
+++ Assets/TrackerObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TrackerObject : MonoBehaviour
+{
+    public static TrackerObject[] trackerObjects = new TrackerObject[4];
+    public Vector3 originOffset;
+    public GameObject cylinderOffset;
+    public Vector3 modelCenterApprox;
+    [SerializeField] public int posIdentifier = 0;
+    public bool visible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrackerObject.trackerObjects[posIdentifier] = this;
+
+        // transform offset to scaled size
+        originOffset = originOffset / transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // cylinderOffset.transform.position = gameObject.transform.position;
+        if (visible) {
+            // modelCenterApprox = gameObject.transform.position + originOffset;
+            // cylinderOffset.transform.rotation = Quaternion.LookRotation(originOffset);
+
+            modelCenterApprox = transform.TransformPoint(originOffset);
+            cylinderOffset.transform.position = modelCenterApprox;
+            cylinderOffset.transform.rotation = Quaternion.LookRotation(transform.up);
+        }
+    }
+
+    void VuforiaTargetLost() {
+        print($"Target {posIdentifier} lost");
+        cylinderOffset.SetActive(false);
+        visible = false;
+    }
+
+    void VuforiaTargetFound() {
+        print($"Target {posIdentifier} found");
+        cylinderOffset.SetActive(true);
+        visible = true;
+    }
+}
