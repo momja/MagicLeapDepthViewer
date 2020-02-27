bad012bb9975c0da10492babf33ee922a9049d84
Max Omdal <mjomdal@gmail.com>
2020-02-27 14:34:47 -0600

    Fixed Offset Behavior for Magic Leap

---
+++ Assets/Scripts/ModelCenter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelCenter : MonoBehaviour
+{
+    Vector3 originalScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalScale = gameObject.transform.localScale;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 avgCenter = Vector3.zero;
+        Vector3 avgLookAtRotation = Vector3.zero;
+        Vector3 avgLookUpRotation = Vector3.zero;
+        int enabledTrackerCount = 0;
+        for (int i = 0; i < TrackerObject.trackerObjects.Length; i++) {
+            TrackerObject trackerObj = TrackerObject.trackerObjects[i];
+            if (trackerObj.visible) {
+                print($"Tracking {trackerObj.posIdentifier}");
+                avgCenter += trackerObj.modelCenterApprox;
+                avgLookAtRotation += trackerObj.transform.up;
+                avgLookUpRotation += trackerObj.transform.forward;
+                enabledTrackerCount += 1;
+            }
+        }
+        if (enabledTrackerCount == 0) {
+            gameObject.transform.localScale = new Vector3(0,0,0);
+            return;
+        }
+        gameObject.transform.localScale = originalScale;
+        avgCenter = avgCenter/enabledTrackerCount;
+        avgLookAtRotation = avgLookAtRotation/enabledTrackerCount;
+        gameObject.transform.localPosition = avgCenter;
+        gameObject.transform.rotation = Quaternion.LookRotation(avgLookUpRotation, avgLookAtRotation);
+    }
+}
---
+++ Assets/Scripts/TrackerObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TrackerObject : MonoBehaviour
+{
+    public static TrackerObject[] trackerObjects = new TrackerObject[6];
+    public Vector3 originOffset;
+    public Vector3 modelCenterApprox;
+    [SerializeField] public int posIdentifier = 0;
+    public bool visible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrackerObject.trackerObjects[posIdentifier] = this;
+        // transform offset to offset in world space
+        originOffset /= transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (visible) {
+            modelCenterApprox = transform.TransformPoint(originOffset);
+        }
+    }
+
+    void VuforiaTargetLost() {
+        print($"Target {posIdentifier} lost");
+        // cylinderOffset.SetActive(false);
+        visible = false;
+    }
+
+    void VuforiaTargetFound() {
+        print($"Target {posIdentifier} found");
+        // cylinderOffset.SetActive(true);
+        visible = true;
+    }
+}
